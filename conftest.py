a1027923717e62628487487106a03b93806bdb02
aakuzmh <tipokot@gmail.com>
2025-08-03 14:10:50 +0300

    added Docker, CI

--- conftest.py
+++ conftest.py
@@ -1,10 +1,18 @@
 import pytest
-import data
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function")
 def driver():
-    data.browser_name = "Chrome"
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.browser_version = "114.0"
+    chrome_options.set_capability("browserName", "chrome")
+    chrome_options.set_capability("enableVNC", True)  # если надо видеть браузер в UI
+    # chrome_options.set_capability("enableVideo", True)  # если хочешь видео
+
+    driver = webdriver.Remote(
+        command_executor="http://selenoid:4444/wd/hub",
+        options=chrome_options
+    )
     yield driver
     driver.quit()
